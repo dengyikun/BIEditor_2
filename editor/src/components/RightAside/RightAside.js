209c7ffda54c5bab91fbeb1404aee7a3b4d9a3de
邓義堃 <dyk@gzqichang.com>
2018-03-07 17:54:50 +0800

    初步完成右侧设置栏

--- editor/src/components/RightAside/RightAside.js
+++ editor/src/components/RightAside/RightAside.js
@@ -2,12 +2,17 @@ import React from 'react';
 import {Icon, Tree} from 'antd';
 import {connect} from 'dva';
 import classNames from 'classnames'
+import Copy from 'react-copy-to-clipboard'
 import {TOOL} from '../../utils'
 import styles from './RightAside.less';
 
 const TreeNode = Tree.TreeNode;
 
-function LeftAside({dispatch, list, activeItem}) {
+function RightAside({dispatch, list, activeItem}) {
+
+  const onCopy = (text, result) => {
+    alert(text + ' || ' +result)
+  }
 
   const getItemNodeList = () => {
 
@@ -49,12 +54,25 @@ function LeftAside({dispatch, list, activeItem}) {
     })
   }
 
-  console.log(activeItem.id)
-
   return (
     <div className={styles.body}>
       <div className={styles.title}>
         基础信息
+      </div>
+      <Copy text={activeItem.id} onCopy={onCopy}>
+        <div className={styles.id}>
+          <label>
+            组件ID<br/>
+            <input placeholder="&#xE648;" disabled
+                   value={activeItem.id || ''}/>
+          </label>
+        </div>
+      </Copy>
+      <div className={styles.name}>
+        <label>
+          组件名称<br/>
+          <input placeholder="&#xE692;" value={activeItem.name || ''}/>
+        </label>
       </div>
       <div className={styles.title}>
         组建设置
@@ -84,4 +102,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(LeftAside)
+export default connect(mapStateToProps)(RightAside)
